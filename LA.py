ce69491856862da90bf8f380d9b974cb9063994c
tannishpage <tannishpage1@gmail.com>
2019-02-07 22:10:39 +1000

    Updating REF and added other functions

--- LA.py
+++ LA.py
@@ -50,19 +50,57 @@ class matrix_opperations:
                     temp_result = 0
         return result
 
-    def REF(self, matrix): #For now assuming all matricies passed are square and don't require any row exchanges
+    def REF(self, matrix, augmented_column):#assume no row exchanges are required
+        #matrix = self.check_row_exchange(matrix, 0)
         matrix_duplicate = matrix
+        aug_col_duplicate = augmented_column
         for x in range(0, len(matrix)):
             piviot = float(matrix[x][x])
             for y in range(x+1, len(matrix[0])):
                 ratio = float(float(matrix[y][x])/piviot)
-                matrix_duplicate[y] = self.row_reduction(matrix[x], matrix[y], ratio)
-        return matrix_duplicate
+                matrix_duplicate[y] = self.row_reduction(matrix_duplicate[x], matrix_duplicate[y], ratio)
+                aug_col_duplicate[y] = self.row_reduction(aug_col_duplicate[x], aug_col_duplicate[y], ratio) 
+            #matrix_duplicate = self.check_matrix_row_exchange(matrix_duplicate)
+        return matrix_duplicate, aug_col_duplicate
+
+    def get_last_column(self, matrix):#assuming matrix is NxN+1
+        last_column = []
+        square_matrix = make_matrix.make_zero_matrix(self, len(matrix), len(matrix))
+        for x in range(0, len(matrix)):
+            for y in range(0, len(matrix)):
+                square_matrix[x][y] = matrix[x][y]
+            last_column.append(matrix[x][len(matrix)])
+        return last_column, square_matrix
+
+    def check_row_exchange(self, matrix, row):
+        if matrix[row][row] == "0":
+            rowB = row + 1
+            new_matrix = self.perform_row_exchange(row, rowB, matrix)
+            return new_matrix
+        else:
+            return matrix
+
+    def check_matrix_row_exchange(self, matrix):
+        new_matrix = []
+        for x in range(0, len(matrix)):
+            new_matrix = self.check_row_exchange(matrix, x)
+        return new_matrix
+
+    def perform_row_exchange(self, rowA, rowB, matrix):
+        rowA_list = matrix[rowA]
+        rowB_list = matrix[rowB]
+        matrix[rowA] = rowB_list
+        matrix[rowB] = rowA_list
+        return matrix
+            
+
+    def RREF(self, matrix):
+        pass
 
     def row_reduction(self, pivoit_row, rowB, ratio):
         reduced_row = []
         for x in range(0, len(rowB)):
-            reduced_row.append(str(int(float(rowB[x]) - (ratio*float(pivoit_row[x])))))
+            reduced_row.append(str(float(float(rowB[x]) - (ratio*float(pivoit_row[x])))))
         return reduced_row
 
     def compute_determinant(self, matrix):
@@ -78,6 +116,26 @@ class matrix_opperations:
             for y in range(0, len(result)):
                 result[y][x] = matrix[x][y]
         return result
+
+    def compute_inverse(self, matrix):#transform into augmented matrix
+        determinant = self.compute_determinant(matrix)
+        identity = make_matrix.make_identity_matrix(len(matrix))
+        if determinant == 0:
+            print("Determinant is 0 cannot compute inverse")
+            return False
+        else:
+            print("Still in development")
+        
+        
+    def make_augmented_matrix(self, matrix):
+        real_matrix = make_matrix.make_zero_matrix(len(matrix[0])/2, len(matrix[0])/2) #Must always be a square matrix
+        augmented_matrix = make_matrix.make_zero_matrix(len(matrix[0])/2, len(matrix[0])/2) #Both real and augmented matrix must be same size
+        for x in range(0, len(matrix[0]/2)):
+            for y in range(0, len(matrix[0]/2)):
+                real_matrix[x][y] = matrix[x][y]
+                augmented_matrix[x][y] = matrix[x][len(matrix[0]/2) + y]
+        return real_matrix, augmented_matrix
+            
 
     def print_matrix(self, matrix):
         for row in matrix:
@@ -102,7 +160,7 @@ class make_matrix:
     def make_random_matrix(self, rows, columns):
         matrix = []
         for x in range(0, rows):
-            row_values = [str(random.randint(0, 100)) for x in range(0, columns)]
+            row_values = [str(random.randint(0, 9)) for x in range(0, columns)]
             matrix.append(row_values)
         return matrix
 
