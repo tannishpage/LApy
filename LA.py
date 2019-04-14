4e6b0cbaca013a6002a5ba6e7bc150359cda2052
tannishpage <tannishpage1@gmail.com>
2019-04-14 12:31:56 +1000

    adding license

--- LA.py
+++ LA.py
@@ -1,3 +1,19 @@
+"""
+                        LICENSE
+This is LApy a Linear Algebra API made with python.
+Copyright (C) 2019  Tannishpage
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 '''
 Plan:
     This is supposed to be an api for Linear Algebra so it is supposed to perform matrix opperations and other vector operations like dot products and cross products.
